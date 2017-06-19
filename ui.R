01e8e2c10bb2a054472aacf46752652c52af2d6f
philippmuench <philipp.muench@helmholtz-hzi.de>
2017-06-19 10:54:50 +0200

    resolve bug on faa file upload

--- ui.R
+++ ui.R
@@ -138,7 +138,7 @@ dashboardPage(skin = "black",
       tabName = "dashboard",
       icon = icon("dashboard")
     ),
-   # menuItem("Start new job", tabName = "newjob", icon = icon("road"),  badgeLabel = textOutput("text3"), badgeColor = "green"),
+    menuItem("Start new job", tabName = "newjob", icon = icon("road"),  badgeLabel = textOutput("text3"), badgeColor = "green"),
     menuItem("Visualize job", tabName = "showjob", icon = icon("pie-chart"),  badgeLabel = textOutput("samplesnum"), badgeColor = "green"),
     conditionalPanel(
       condition = "input.sid == 'showjob'",
